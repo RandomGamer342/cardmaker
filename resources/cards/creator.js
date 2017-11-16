dc520ae64d005403cf823c935319a8c20168c77f
Peder Bergebakken Sundt <pbsds@hotmail.com>
2017-11-16 22:35:54 +0100

    Make creator preview update after timeout, instead of each keyup

--- resources/cards/creator.js
+++ resources/cards/creator.js
@@ -1,5 +1,8 @@
-function submit_preview() {
-    document.getElementById("cardform").submit();
+function submit_preview(form) { /* with timeout for ratelimiting*/
+    if (typeof refresh_timeout !== 'undefined') {
+        window.clearTimeout(refresh_timeout);
+    }
+    refresh_timeout = window.setTimeout(function(){form.submit();}, 200);
 }
 
-window.setTimeout(submit_preview, 1);
+window.setTimeout(function(){document.getElementById("cardform").submit();}, 1);
